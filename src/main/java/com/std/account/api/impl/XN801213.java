756301c9cf549d63b76bad89c1078f68a9d93a10
EllenWuql <604690332@qq.com>
2016-01-14 20:29:58 +0800

    update

--- src/main/java/com/std/account/api/impl/XN801213.java
+++ src/main/java/com/std/account/api/impl/XN801213.java
@@ -5,6 +5,7 @@ import com.std.account.api.AProcessor;
 import com.std.account.common.JsonUtil;
 import com.std.account.core.StringValidater;
 import com.std.account.dto.req.XN801204Req;
+import com.std.account.dto.req.XN801213Req;
 import com.std.account.exception.BizException;
 import com.std.account.exception.ParaException;
 import com.std.account.spring.SpringContextHolder;
