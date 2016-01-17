a5cae431449c2c0d1691be2e7d2fcc6ffb51ae14
myb858 <myb858@hotmail.com>
2016-01-17 19:33:52 +0800

    update

--- src/main/java/com/std/account/api/impl/XN802612.java
+++ src/main/java/com/std/account/api/impl/XN802612.java
@@ -3,7 +3,6 @@ package com.std.account.api.impl;
 import com.std.account.ao.IAJourAO;
 import com.std.account.api.AProcessor;
 import com.std.account.common.JsonUtil;
-import com.std.account.core.StringValidater;
 import com.std.account.dto.req.XN802612Req;
 import com.std.account.exception.BizException;
 import com.std.account.exception.ParaException;
@@ -29,7 +28,5 @@ public class XN802612 extends AProcessor {
     @Override
     public void doCheck(String inputparams) throws ParaException {
         req = JsonUtil.json2Bean(inputparams, XN802612Req.class);
-        StringValidater.validateBlank(req.getHlNo(), req.getApproveUser(),
-            req.getApproveResult(), req.getApproveNote());
     }
 }
