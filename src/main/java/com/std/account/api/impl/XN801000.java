cb1219b394ff5eb4bf6a87836ce154c4c4a56bd6
myb858 <myb858@hotmail.com>
2016-01-18 16:57:23 +0800

    update

--- src/main/java/com/std/account/api/impl/XN801000.java
+++ src/main/java/com/std/account/api/impl/XN801000.java
@@ -5,8 +5,8 @@ import com.std.account.api.AProcessor;
 import com.std.account.common.JsonUtil;
 import com.std.account.common.PhoneUtil;
 import com.std.account.core.StringValidater;
-import com.std.account.dto.req.XN801100Req;
-import com.std.account.dto.res.XN801100Res;
+import com.std.account.dto.req.XN801000Req;
+import com.std.account.dto.res.XN801000Res;
 import com.std.account.exception.BizException;
 import com.std.account.exception.ParaException;
 import com.std.account.spring.SpringContextHolder;
@@ -20,17 +20,17 @@ import com.std.account.spring.SpringContextHolder;
 public class XN801000 extends AProcessor {
     private ISmsOutAO smsOutAO = SpringContextHolder.getBean(ISmsOutAO.class);
 
-    private XN801100Req req = null;
+    private XN801000Req req = null;
 
     @Override
     public Object doBusiness() throws BizException {
         smsOutAO.sendCaptcha(req.getMobile(), req.getBizType());
-        return new XN801100Res(true);
+        return new XN801000Res(true);
     }
 
     @Override
     public void doCheck(String inputparams) throws ParaException {
-        req = JsonUtil.json2Bean(inputparams, XN801100Req.class);
+        req = JsonUtil.json2Bean(inputparams, XN801000Req.class);
         StringValidater.validateBlank(req.getMobile(), req.getBizType());
         PhoneUtil.checkMobile(req.getMobile());// 判断格式
     }
