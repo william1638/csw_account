e8af5c76b241d94ad7933a4b0fda001f727ac5e8
pyez1158 <pyez1158@163.com>
2016-04-08 14:01:55 +0800

    发包版本号更改

--- src/main/java/com/std/account/APIServlet.java
+++ src/main/java/com/std/account/APIServlet.java
@@ -41,8 +41,8 @@ public class APIServlet extends HttpServlet {
             HttpServletResponse response) throws ServletException, IOException {
         logger.info("Successful Deployment");
         PrintWriter writer = response.getWriter();
-        writer.append("Version:1.1.0 \n");
-        writer.append("Description:\n");
+        writer.append("Version:1.2.0 \n");
+        writer.append("Description:第三次发包\n");
         writer.flush();
     }
 
