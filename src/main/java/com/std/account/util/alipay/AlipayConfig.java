82c36e131d228a9fa956552298a42e42fa1c64d3
haiqingzheng <leo.zheng@ommo.cn>
2017-01-15 14:27:55 +0800

    支付宝对接，未测试

---
+++ src/main/java/com/std/account/util/alipay/AlipayConfig.java
@@ -0,0 +1,18 @@
+/**
+ * @Title AlipayConfig.java 
+ * @Package com.std.account.util.alipay 
+ * @Description 
+ * @author haiqingzheng  
+ * @date 2017年1月12日 上午10:29:29 
+ * @version V1.0   
+ */
+package com.std.account.util.alipay;
+
+/** 
+ * @author: haiqingzheng 
+ * @since: 2017年1月12日 上午10:29:29 
+ * @history:
+ */
+public class AlipayConfig {
+
+}
