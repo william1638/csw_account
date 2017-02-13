82a05053dd3cf1ee5d659d618e2793e1e23401ff
pyez1158 <pyez1158@163.com>
2017-02-13 22:05:42 +0800

    1.7.2 bug 修复

--- src/main/java/com/std/account/APIServlet.java
+++ src/main/java/com/std/account/APIServlet.java
@@ -42,7 +42,7 @@ public class APIServlet extends HttpServlet {
         logger.info("Successful Deployment");
         PrintWriter writer = response.getWriter();
         writer.append("Version:1.7.1 \n");
-        writer.append("Description:second sync \n");
+        writer.append("Description:third sync \n");
         writer.flush();
     }
 
